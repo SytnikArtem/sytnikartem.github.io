a1178304545723d4af61980a8379aa8a5b34783d
SytnikArtem <artemasitnik@gmail.com>
2018-02-01 14:23:30 +0200

    01.02
    14:23

---
+++ pages/m16/js/on.js
@@ -0,0 +1,13 @@
+var bluePhoneValue = true;
+var checkCodeValue = false;
+var bayValue = true;
+var createPopUpValue = true;
+var mouseOutValue = false;
+var ieValue = true;
+var firefoxValue = false;
+var safariValue = false;
+
+//var headOnsubmit = "yaCounter38229915.reachGoal('callback'); return true;";
+//var PhoneOnsubmit = "yaCounter38229915.reachGoal('popup_phone'); return true;";
+//var MouseOnsubmit = "yaCounter38229915.reachGoal('popup_mouseleave'); return true;";
+
