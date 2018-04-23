246ec47ad83bb0b7210ebe356dbc63c93547304c
SytnikArtem <artemasitnik@gmail.com>
2018-04-23 18:00:26 +0300

    24.03
    18:00

--- pages/maps/js/main.js
+++ pages/maps/js/main.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
   var countryCode = prompt('Код страны', 'UA');
   var countryBlock = (".block_" + countryCode).toLowerCase();
   $('.country-block').find('.land').css({"fill": colorMaps, "stroke": colorMaps});
+  $('.country-block').find('.water').css({"fill": "transparent", "stroke": "transparent"});
   $(countryBlock).css("display", "block");
 });
