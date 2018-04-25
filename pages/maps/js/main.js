3e708b964f10aa4ef453613ae3ac9d3f66f38d1a
SytnikArtem <artemasitnik@gmail.com>
2018-04-25 11:30:43 +0300

    25.04
    11:30

--- pages/maps/js/main.js
+++ pages/maps/js/main.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
   $('.country-block').find('.land').css({"fill": colorMaps, "stroke": colorMaps});
   $('.country-block').find('.water').css({"fill": "transparent", "stroke": "transparent"});
   $(countryBlock).css("display", "block");
+  var land = $(".land");
+  land.mouseover(function(){
+    land.removeClass("active");
+    this.classList.add("active");
+    var dataRegion = this.hasAttribute('data-region');
+    if (dataRegion) {
+      for(let i = 0; i < land.length; i++) {
+        if(land[i].getAttribute('data-region') == this.getAttribute('data-region')) {
+          land[i].classList.add('active');
+        }
+      }
+    }
+  })
 });
