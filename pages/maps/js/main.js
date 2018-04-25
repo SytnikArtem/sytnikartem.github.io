88a0b27f9907e2a215a9bfdfa206fd75c1190b1d
SytnikArtem <artemasitnik@gmail.com>
2018-04-25 12:35:47 +0300

    25.04
    12:35

--- pages/maps/js/main.js
+++ pages/maps/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var colorMaps = prompt('Цвет карты', 'red');
   var countryCode = prompt('Код страны', 'UA');
+  var colorLocation = prompt('Цвет города', 'yellow')
   var countryBlock = (".block_" + countryCode).toLowerCase();
   $('.country-block').find('.land').css({"fill": colorMaps, "stroke": colorMaps});
   $('.country-block').find('.water').css({"fill": "transparent", "stroke": "transparent"});
@@ -17,5 +18,7 @@ $(document).ready(function(){
         }
       }
     }
-  })
+    $('.land').css({"fill": colorMaps, "stroke": colorMaps})
+    $('.land.active').css({"fill": colorLocation, "stroke": colorLocation})
+  });
 });
