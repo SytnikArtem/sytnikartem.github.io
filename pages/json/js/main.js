b7d53d20f228f024b3db2588f06ca6f7ae2d641b
SytnikArtem <artemasitnik@gmail.com>
2018-05-24 17:14:32 +0300

    111
    111

--- pages/json/js/main.js
+++ pages/json/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     function detect(code){
         console.log('re');
         console.log(code);
-        $.getJSON('th.json', function(arr) {
+        $.getJSON(code, function(arr) {
         let titleLocation;
         console.log('re');
         let land = $(".land");
