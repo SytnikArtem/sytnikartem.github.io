c47e822434cc9c90e6ce741b5e55c207acec84cc
Artem Sytnik <sytnik.a@brights.cloud>
2019-09-10 11:07:34 +0300

    add my porfolio vue
    add my porfolio vue

---
+++ pages/my-portfolio/js/chunk-2d217e3c.21358074.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217e3c"],{c93c:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n.$createElement,c=n._self._c||t;return c("div",{staticClass:"main-container contacts"},[n._v("Contacts")])},a=[],s={name:"Contacts"},o=s,i=c("2877"),r=Object(i["a"])(o,e,a,!1,null,"21650e23",null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d217e3c.21358074.js.map
